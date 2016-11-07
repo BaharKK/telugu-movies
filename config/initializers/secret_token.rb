a29314c6e7afce248bcde224f467fde84b457299
BaharKK <bahar_rachapalli@yahoo.com>
2016-11-06 18:30:07 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeluguMovies::Application.config.secret_key_base = 'c9dc3d49077079dc870ce037c39c84cde719d017b812001d13699799ac1e95420d410d6c5ce2dddd4cec51db2df4a7fe02467538abb1c816dcc7307b7dcdbc3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeluguMovies::Application.config.session_store :cookie_store, key: '_telugu-movies_session'
